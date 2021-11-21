40c151ad5fc242f06c98df33cd410836f65897c7
JERATHEL JEAN <76412220+jera66@users.noreply.github.com>
2021-11-20 22:08:56 -0500

    backend first commit

---
+++ readme.md
@@ -0,0 +1,115 @@
+# Venting Forum
+
+#### By Quentin Bartholomew, Jerathel Jean, and Ras Au-t Amam
+
+## Project Summary
+
+This project is a forum with a theme based around venting, whether that be about programming or just life. The page will have a social media style display to give users a familiar feel.  This forum project is a three-person group project and each person gets assigned a specific task  to work on.
+
+
+<a href="https://compassionate-ritchie-5e01af.netlify.app/">Live link</a>
+
+### Technologies used
+
+1- Express/Node
+2- Mongo/Mongoose
+3- Dotenv
+4- Cors
+5- Morgan
+6- React
+
+### List of dependencies
+
+```js
+require('dotenv').config()
+const { PORT = 3002, DATABASE_URL } = process.env
+const express = require('express')
+const app = express()
+const mongoose = require('mongoose')
+const cors = require('cors')
+const morgan = require('morgan')
+```
+
+### Model
+
+```js
+// The forum schema
+const ForumSchema = new mongoose.Schema(
+{
+userName: String,
+about: String,
+date: Date,
+time: String,
+message: String,
+url: String,
+startRating: Number
+
+},
+{ timestamps: true }
+)
+// Creating the forum model
+const Messages = mongoose.model('Messages', ForumSchema)
+
+```
+
+### Middleware
+
+```Js
+/_ MIDDLEWARE _/
+app.use(cors())
+app.use(morgan('dev'))
+app.use(express.json())
+```
+
+
+
+
+### Backend Route Table
+
+| url                | method | action                                   |
+| --------------- | ------   | -----------------------------------|
+| /message/     | get       | get all the messages (index) |
+| /message      | post     | create a new message           |
+| /message/:id | put      | update a message                  |
+| /message/:id | delete  | delete a message                   |
+
+
+## User Stories
+
+A user should be greeted with a welcome message on the homepage
+
+A user should be able to view all of the messages currently posted on the forum
+
+A user should be able to post a new message to the forum
+
+A user should be able to edit a message on the forum
+
+A user should be able to delete a message on the forum
+
+// Potential user stories beyond MVP //
+
+A user should be able to log in
+
+A user should only be able to delete their own messages
+
+A user should only be able to edit their own messages
+
+A user should be able to 'upvote' a message posted on the forum
+
+A user should see the 'most popular' message - the message with the most upvotes
+
+## Challenges
+This time I just didn't know where exactly to start because I felt kind of overwhelmed, but everything else went well.
+
+## List of Technologies
+
+- CSS
+
+- JS/JSX/JQuery
+
+- Express
+
+- React
+
+- Mongoose
+
--- server.js
+++ server.js
@@ -1,33 +1,117 @@
-/*          DEPENDENCIES            */
+// ///////////////////////////
+// Dependencies
+// ///////////////////////////
+
+// Getting .env variables
 require('dotenv').config()
-const {PORT = 3002, DATABASE_URL} = process.env
+// Pulling PORT from .env, giving it a default of 3002 (object destructuring)
+const { PORT = 3002, DATABASE_URL } = process.env
+// Importing express
 const express = require('express')
+// Creating  the application object
 const app = express()
+// Importing mongoose
 const mongoose = require('mongoose')
+// Importing middleware
 const cors = require('cors')
 const morgan = require('morgan')
 
-/*          DATABASE CONNECTION         */
+// ///////////////////////////////
+// Database Connection
+// //////////////////////////////
+// Establishing connection
 mongoose.connect(DATABASE_URL, {
-    useUnifiedTopology: true,
-    useNewUrlParser: true
+  useUnifiedTopology: true,
+  useNewUrlParser: true
 })
 
+// Connection Events
 mongoose.connection
-.on('open', () => {console.log('Connected to Mongo')})
-.on('close', () => {console.log('Disconnected from Mongo')})
-.on('error', (error) => {console.log(error)})
+  .on('open', () => console.log('You are connected to Mongo'))
+  .on('close', () => console.log('You are disconnected from Mongo'))
+  .on('error', error => console.log(error))
 
-/*          MIDDLEWARE          */
-app.use(cors())
-app.use(morgan('dev'))
-app.use(express.json())
+// ////////////////////////////
+// Models
+// ////////////////////////////
+// The forum  schema
+const ForumSchema = new mongoose.Schema(
+  {
+    userName: String,
+    about: String,
+    date: Date,
+    time: String,
+    message: String,
+    url: String,
+    startRating: Number
 
-/*          ROUTES          */
+  },
+  { timestamps: true }
+)
+// Creating the forum model
+const Messages = mongoose.model('Messages', ForumSchema)
+
+// ///////////////////////////////
+// Middleware
+// ////////////////////////////////
+app.use(cors()) // Preventing cors errors, opening up access for frontend
+app.use(morgan('dev')) // Logging
+app.use(express.json()) // Parsing json bodies
+
+// //////////////////////////////
+// Routes
+// //////////////////////////////
+// Setting up a test route
 app.get('/', (req, res) => {
-    res.send('Hello World')
+  res.send('Hello World!')
 })
 
+// Forum index route
+// Getting request to /forum messages, returning them all as json
+app.get('/messages', async (req, res) => {
+  try {
+    // Sending all forum messages
+    res.json(await Messages.find({}))
+  } catch (error) {
+    res.status(400).json({ error })
+  }
+})
+//  Forum create route
+// Posting request to /forum, using request body to make new forum messages
+app.post('/messages', async (req, res) => {
+  try {
+    // Creating a new forum message
+    res.json(await Messages.create(req.body))
+  } catch (error) {
+    res.status(400).json({ error })
+  }
+})
+// Forum update  route
+// Putting request /message/:id, updates messages based on id with request body
+app.put('/messages/:id', async (req, res) => {
+  try {
+    // Updating a message
+    res.json(
+      await Messages.findByIdAndUpdate(req.params.id, req.body, { new: true })
+    )
+  } catch (error) {
+    res.status(400).json({ error })
+  }
+})
+// Destroy Route
+// Deleting request to /message/:id, deletes messages specified
+app.delete('/messages/:id', async (req, res) => {
+  try {
+    // Deleting a message
+    res.json(await Messages.findByIdAndRemove(req.params.id))
+  } catch (error) {
+    res.status(400).json({ error })
+  }
+})
 
-/*          SERVER LISTENER         */
-app.listen(PORT, () => {console.log(`Listening on port ${PORT}`)})
+// ///////////////////////////////
+// Server Listener
+// ///////////////////////////////
+app.listen(PORT, () => {
+  console.log(`listening on PORT ${PORT}`)
+})
